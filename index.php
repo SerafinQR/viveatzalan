4fe0aefde690370e2fae6e38334a8bf5f58066ba
SerafinQR <qurs9314@gmail.com>
2019-09-27 15:35:07 -0500

    segunda pagina web

---
+++ index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="esp">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Atzalan"/>
+<meta name="Keywords" content="atzalan,magico,totonacapan,pueblo magico" />
+<title>Atzalan pueblo magico</title>
+<link href="estilos/estilos.css" rel="stylesheet" type="text/css">
+<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.--><script>var __adobewebfontsappname__="dreamweaver"</script><script src="http://use.edgefonts.net/montserrat:n4:default;source-sans-pro:n2:default.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="mainwrapper">
+  <header> 
+    <!--**************************************************************************
+    Header starts here. It contains Logo and 3 navigation links. 
+    ****************************************************************************-->
+    <div id="logo"><img src="estilos/logo atzalan.png" alt="sample logo"><!-- Company Logo text -->Tierra del totonacapan</div>
+    <nav> <a href="#" title="Link">Mapas</a> <a href="#" title="Link">Lugares turisticos</a><a href="#" title="Link">Gobierno</a> </nav>
+  </header>
+  <div id="content">
+    <div class="notOnDesktop"> 
+      <!-- This search box is displayed only in mobile and tablet laouts and not in desktop layouts -->
+      <input type="text" placeholder="Search">
+    </div>
+    <section id="mainContent"> 
+      <!--************************************************************************
+    Main Blog content starts here
+    ****************************************************************************-->
+      <h1><!-- Blog title -->Historia</h1>
+      <h3><!-- Tagline -->Bienvenido al mundo del totonacapan.</h3>
+      <div id="bannerImage"><img src="estilos/torre.jpg" alt=""/></div>
+      <p>Del náhuatl. Atl, a; agua, Tsalan; en medio, entre. "Entre aguas", alude a los arroyos que rodean a su actual cabecera.</p>
+		
+		<p>Sobre fondo argen (blanco) se destaca en forma exabada con un contorno azul en el cual se encuentra en la parte superior una estrella amarilla de cinco puntos, un batracio en cada lado denominado calate y la leyenda Atzalan.
+
+		El campus está dividido en dos por una línea horizontal, en la pieza principal se encuentra un paisaje con cuatro cerros, destacando al centro la vieja torre solitaria del campanario y a su lado una casa. En la pieza secundaria sobre un fondo café, se encuentra el glifo que describe a Atzalan.
+
+		Este escudo se encuentra coronado por dos ramas de cafeto en producción de cada lado, al centro se encuentra un libro abierto cruzado por una pluma y con la inscripción MDXIX, , sobresaliendo en la parte superior una antorcha encendida.</p>
+		
+		<p>En la época prehispánica fue una aldea totonaca de la Confederación de Mexicaltzingo. Altotonga, hoy municipio, llego a pertenecer a Atzalan. En 1569, siendo vicario de la provincia de Atzalán Fray Diego de López, se construyó la capilla de San Miguel y en 1670 el templo principal.
+
+		Lo que ahora es la villa de Atzalan, fué fundada por Mexcaltecutli señor del Mezcalzingo hacia el año 1200. La fundación de Atzalan, se debió a motivos militares, pues se había dado la penetración nahua, principalmente, en Xalacingo y Nautla. El maestro David Ramirez Lavoignet en su libro Tlapacoyan establece: que el pueblo de Atzalan, originalmente pueblo confederado, queda convertido en aldea a raíz de la Conquista, estableciéndose aqui la nueva cabecera del señorío. En 1554 lo visita Diego Huitziquitzin a nombre del Virrey, dando en él posesión del cacicazgo a Don Pedro Fernández Pimentel, descendiente del señor del Mezcalcingo.</p>
+		
+		<p>Durante la conquista española, Atzalan fué de los primeros pueblos que recibieron la influencia del conquistador, el padre Francisco María Cortés, en su monografía de la parroquia de San Andrés Atzalan, establece que en el año 1639 se construyó la primera iglesia de este pueblo, la torre solitaria del campanario se construyó en el año de 1702.
+
+		En el siglo pasado, en los años ochentas, siendo jefe político del cantón de Jalacingo Don Agustín Romo, se trasladó al pueblo de Atzalan los poderes cantonales o sea el juzgado de primera instancia, la agencia del ministerio público, el registro público de la propiedad., el reclusorio y la guardia de rurales; en esta época se inició la construcción del palacio municipal sobre lo que se conocia, desde la época de la colonia, como casas reales, la cual fue terminada en 1901.
+
+		Los poderes cantonales duraron en Atzalan dos años, debido a la influencia de la familia Villegas asentada en Jalacingo éstos retornaron a esa población. Durante este tiempo, el pueblo de Atzalan tuvo un resurgimiento económico por encima de las poblaciones aledañas, así el primero de diciembre de 1891, el pueblo de Atzalan fué elevado a la categoria de villa, el cual todavía sustenta; para recordar este hecho, existe una placa conmemorativa que se encuentra incrustada en el kiosco del parque Colón.
+
+		Con el advenimiento de la revolución los capitales que había en Atzalan desaparecieron, lo que la aletargó. Durante la época revolucionaria tocó a Atzalan tener el honor de haber sido testigo de la primera restitución de tierras hecha legalmente, es decir, que una vez promulgada la ley del 6 de enero de 1915, las comunidades de La Florida, Sompázol, Tatzallanala, Ictzictic y Xontaxpan recobraron las tierras de las cuales habían sido despojados y que detentaba el licenciado Benigno Ríos, propietario de la hacienda de Santa Cruz; esta restitución fué hecha por el teniente de armas</p>
+		
+		<p>El espacio siguiente es para agregar comentarios o quejas</p>
+      <aside id="authorInfo"> 
+        <!-- The author information is contained here -->
+        <h2>vive atzalan</h2>
+      </aside>
+    </section>
+    <section id="sidebar"> 
+      <!--************************************************************************
+    Sidebar starts here. It contains a searchbox, sample ad image and 6 links
+    ****************************************************************************-->
+      <input type="text" placeholder="Search">
+      <div id="adimage"><img src="BlogPostAssets/images/300x250.gif" alt=""/></div>
+      <nav>
+        <ul>
+          <li><a href="#" title="Link">Rutas</a></li>
+          <li><a href="#" title="Link">Arqueologia</a></li>
+          <li><a href="#" title="Link">Zona alta</a></li>
+          <li><a href="#" title="Link">Zona media </a></li>
+          <li><a href="#" title="Link">Zona baja</a></li>
+        </ul>
+      </nav>
+    </section>
+    <footer> 
+      <!--************************************************************************
+    Footer starts here
+    ****************************************************************************-->
+      <article>
+        <h3>Générer du changement</h3>
+        <p>Mejorar es cambiar; así que para ser perfecto hay que haber cambiado a menudo. (Winston Churchill)</p>
+      </article>
+      <article>
+        <h3>Denuncia</h3>
+       <p> Puedes tambien marcar a los numeros de comandancia municipal de Atzalar,ver Tel:(226)3160935 ó (226)3160976 </p>
+      </article>
+    </footer>
+  </div>
+  <div id="footerbar"><!-- Small footerbar at the bottom --></div>
+</div>
+</body>
+</html>
+
